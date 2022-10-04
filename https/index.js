8a8f07a79df759443315b8d72faf7e62fb34bbf0
a.trsk <anntarasiuk@gmail.com>
2022-10-04 23:58:11 +0300

    Add files via upload

---
+++ https/index.js
@@ -0,0 +1,45 @@
+const https = require('https');  //  подключаем модуль https
+const fs = require('fs');
+
+const options = {                 //  сертификат
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, function (req, res) {
+    res.setHeader("Content-Type", "text/html; charset=utf-8;");
+    
+    const pathReq = req.url;  //  получаем маршрут 
+    console.log(pathReq);
+    
+    if(req.method="GET" && pathReq == "/")  //  get - запрос
+    {
+        
+        fs.readFile("./index.html", function(error, data){  //  отдаем index.html
+              
+            if(error){
+                      
+                res.statusCode = 404;
+                res.end("Resourse not found!");
+            }   
+            else{
+                res.end(data);
+            }
+        });
+        
+    }
+    else if(req.method="POST" && pathReq=="/login")  //  post - запрос
+    {
+        let body = "";
+        req.on("data", (chunk)=>{body += chunk});
+        req.on("end",()=>{
+            const query = parse(body);
+            console.log("Login: ${query.login} Password: ${query.password}");  //  выводим в консоль
+            res.write("<h1>Авторизация успешна</h1>");  //  выводим текст для пользоателя
+            res.end();
+        })
+    }
+    else{
+        res.end();
+    }
+}).listen(8000);
